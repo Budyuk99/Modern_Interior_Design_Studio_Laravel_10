98f2388d619f5f93ec7fd0e3c707631e56d53492
Nikita Budyuk <57536202+Budyuk99@users.noreply.github.com>
2024-09-07 13:32:12 +0300

    Created laravel project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    });
+    
+    Route::get('/shop', function () {
+        return view('shop');
+    });
 });
